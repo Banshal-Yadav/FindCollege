a1e257fe5749eb1b19bac21a29cf9de924a6a0cd
Banshal-Yadav <banshalyadav99@gmail.com>
2025-03-10 02:19:25 -0700

    Add process.env.PORT fallback (default to 3000) for port configuration

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcrypt');
 const session = require('express-session');
 const app = express();
 app.use(express.static('public'));
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
